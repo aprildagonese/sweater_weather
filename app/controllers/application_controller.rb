4f0f4ab910e41cd4a443c3e5523e181490afb416
April Dagonese <adagonese@gmail.com>
2019-04-14 15:10:57 -0600

    Removes CSRF

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,6 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
   helper_method :current_user
-
-  # def current_user
-  #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  # end
 
   def unauthorized
     { error: "API key required" }
