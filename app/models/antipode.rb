99ac5d044ded0535224d8621991d170419d7ecc5
April Dagonese <adagonese@gmail.com>
2019-04-15 10:24:54 -0600

    cleaning up antipode object

--- app/models/antipode.rb
+++ app/models/antipode.rb
@@ -1,29 +1,46 @@
 class Antipode
   def initialize(location)
+    prettify_data(location)
+  end
+
+  def prettify_data(location)
+    {
+        "data": [
+            {
+                "id": "1",
+                "type": "antipode",
+                "attributes": {
+                    "location_name": "Antipode City Name",
+                    "forecast": {
+                        "summary": "Mostly Cloudy,
+                        "current_temperature": "72",
+                    				},
+                "search_location": "Hong Kong"
+                }
+            }
+        ]
+    }
+  end
+
+  def geocode(location)
     geocode = GeocodingService.new
     geocode.geocode(location)
-    lat_long = geocode.lat_long
-    antipode_data = AmypodeService.new.fetch_antipode(lat_long[:lat], lat_long[:lng])
-    lat = antipode_data[:data][:attributes][:lat]
-    long = antipode_data[:data][:attributes][:long]
-    antipode_location = GeocodingService.new.reverse_geocode(lat, long)
+    geocode.lat_long
+  end
+
+  def antipode_data(lat_long)
+    @antipode_data ||= AmypodeService.new.fetch_antipode(geocode(location)[:lat], geocode(location)[:lng])
+  end
+
+  def antipode_lat
+    @antipode_data[:data][:attributes][:lat]
+  end
+
+  def antipode_long
+    @antipode_data[:data][:attributes][:long]
+  end
+  
+  def reverse_geocode(antipode_lat, antipode_long)
+    GeocodingService.new.reverse_geocode(antipode_lat, antipode_long)
   end
 end
-
-
-#{
-#     "data": [
-#         {
-#             "id": "1",
-#             "type": "antipode",
-#             "attributes": {
-#                 "location_name": "Antipode City Name",
-#                 "forecast": {
-#                     "summary": "Mostly Cloudy,
-#                     "current_temperature": "72",
-#                 				},
-#             "search_location": "Hong Kong"
-#             }
-#         }
-#     ]
-# }
--- app/services/geocoding_service.rb
+++ app/services/geocoding_service.rb
@@ -2,35 +2,36 @@ class GeocodingService
   attr_reader :country, :state, :city, :lat_long
 
   def reverse_geocode(lat, long)
-    location_name(lat, long)
+    reverse_location_data(lat, long)
   end
 
   def geocode(location)
-    set_location_data(location)
+    location_data(location)
   end
 
-  def set_location_data(location)
-    if geo_info(location).count == 1
-      @lat_long = forecast(location)[:results][0][:geometry][:location]
-      @long_name = forecast(location)[:results][0][:address_components][0][:long_name]
-    else
-      @country = geo_info(location)[3][:long_name]
-      @state = geo_info(location)[2][:short_name]
-      @city = geo_info(location)[0][:short_name]
-      @lat_long = forecast(location)[:results][0][:geometry][:location]
-    end
+  def location_data(location)
+    @country = geo_info(location)[3][:long_name]
+    @state = geo_info(location)[2][:short_name]
+    @city = geo_info(location)[0][:short_name]
+    @lat_long = forecast(location)[:results][0][:geometry][:location]
+  end
+
+  def reverse_location_data(lat, long)
+    data = reverse_location(lat, long)
+    @lat_long = forecast(location)[:results][0][:geometry][:location]
+    @long_name = forecast(location)[:results][0][:address_components][0][:long_name]
   end
 
   def geo_info(location)
     forecast(location)[:results][0][:address_components]
   end
 
-  def forecast(location)
-    @forecast_data ||= json("?address=#{location}")
+  def location(location)
+    @location ||= json("?address=#{location}")
   end
 
-  def location_data(lat, long)
-    @location_name ||= json("?latlng=#{lat},#{long}")
+  def reverse_location(lat, long)
+    @reverse_location ||= json("?latlng=#{lat},#{long}")
   end
 
   def json(url)
