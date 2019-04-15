b0734b4b2f79c6ba939f95104604e52bc1c98fa7
April Dagonese <adagonese@gmail.com>
2019-04-15 10:55:18 -0600

    Still working with antipode object

--- app/models/antipode.rb
+++ app/models/antipode.rb
@@ -1,9 +1,10 @@
 class Antipode
   def initialize(location)
-    prettify_data(location)
+    @_location = location
+    prettify_data
   end
 
-  def prettify_data(location)
+  def prettify_data
     {
         "data": [
             {
@@ -11,10 +12,7 @@ class Antipode
                 "type": "antipode",
                 "attributes": {
                     "location_name": "#{antipode_name}",
-                    "forecast": {
-                        "summary": "Mostly Cloudy",
-                        "current_temperature": "72",
-                    				},
+                    "forecast": antipode_forecast,
                 "search_location": "Hong Kong"
                 }
             }
@@ -22,36 +20,52 @@ class Antipode
     }
   end
 
-  def geocode(location)
-    geocode = GeocodingService.new
-    geocode.geocode(location)
-    geocode.lat_long
+  def service
+    @geo_service ||= GeocodingService.new
+    @geo_service.geocode(location)
+    @geo_service
   end
 
-  def antipode_data(lat_long)
-    @antipode_data ||= AmypodeService.new.fetch_antipode(geocode(location)[:lat], geocode(location)[:lng])
+  def location_lat_long
+    binding.pry
+    service.lat_long
+  end
+
+  def antipode_data
+    @antipode_data ||= AmypodeService.new.fetch_antipode(location_lat_long[:lat], location_lat_long[:lng])
   end
 
   def antipode_lat
-    @antipode_data[:data][:attributes][:lat]
+    antipode_data[:data][:attributes][:lat]
   end
 
   def antipode_long
-    @antipode_data[:data][:attributes][:long]
+    antipode_data[:data][:attributes][:long]
   end
 
-  def reverse_geocode(antipode_lat, antipode_long)
+  def reverse_geocode
     @reverse_geocode ||= GeocodingService.new.reverse_geocode(antipode_lat, antipode_long)
   end
 
   def antipode_name
-    @reverse_geocode.long_name
+    reverse_geocode.long_name
   end
 
   def antipode_lat_long
-    @reverse_geocode.lat_long
+    reverse_geocode.lat_long
   end
 
-  def forecast
+  def antipode_forecast
+    {
+      "summary": "Mostly Cloudy",
+      "current_temperature": "72",
+    }
+  end
+
+  private
+  attr_reader :location
+
+  def location
+    @_location
   end
 end
