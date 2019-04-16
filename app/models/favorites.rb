2b96d9123e6d9059930f512c0b93da20f45aaed2
April Dagonese <adagonese@gmail.com>
2019-04-16 09:14:25 -0600

    fixes regression from facades

--- app/models/favorites.rb
+++ app/models/favorites.rb
@@ -1,7 +1,7 @@
 class Favorites
   def self.generate(user_cities)
     user_cities.map do |user_city|
-      Forecast.new(user_city.city.location)
+      ForecastFacade.new(user_city.city.location)
     end
   end
 end
